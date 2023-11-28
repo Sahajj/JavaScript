2e2fc845adf8d08091cd55050a011bb9a47d7b10
sahaj9 <sahajjain739739@gmail.com>
2023-11-28 10:35:48 +0530

    updated file structure and added readme file

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 144553
+// to declare variables in js we can use two keywords let and var
+let accountEmail = "sahaj@gmail.com"
+// var is not used in morden js
+/*
+Prefer Not to use var
+Because of issue in block scope nda functional scope
+*/ 
+var accountPassword = "12345"
+
+// Not to be used BUT CAN BE USED LIKE THIS
+accountCity = "Jaipur"
+
+// In js if you define any variable and don't give it any value 
+// It will be saved as undefined
+let accountState;
+
+// accountId = 2 // Not allowed
+
+accountEmail = "sj@sj.com"
+accountPassword = "2213121"
+accountCity = "Delhi"
+
+console.log(accountId);
+// Better way to print when you have a lot of variables to print
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
--- L_1/script.js
+++
@@ -1,8 +0,0 @@
-// giives alert on the web page 
-alert("Hello mom From method 3");
-
-// writes in the html doc
-document.write("Hello Mom from method 3");
-
-// consol log 
-console.log("Hello Mom from method 3");
