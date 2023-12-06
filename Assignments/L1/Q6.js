27699cdaa73367c588bb02c2292497204b6a198d
sahaj9 <sahajjain739739@gmail.com>
2023-12-06 23:44:52 +0530

    Added another question

--- Assignments/L1/Q6.js
+++ Assignments/L1/Q6.js
@@ -1,1 +1,16 @@
 // WAP to input n numbers and log the average of those number.
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+function calculateAverage(array) {
+    let total = 0;
+    let count = 0;
+
+    array.forEach(function(item, index) {
+        total += item;
+        count++;
+    });
+
+    return total / count;
+}
+
+console.log(calculateAverage(array));
